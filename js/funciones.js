f81a1303a4df3a5635d0353083b0d6038b30aad9
javier cerda ibañez <javiercerdaibaez@gmail.com>
2022-12-09 18:41:10 +0100

    casi todo de la ultima de js

--- js/funciones.js
+++ js/funciones.js
@@ -2,12 +2,18 @@ addEventListener('load', (event) => {
     table_load();
 })
 const elementos = [
-    {"nombre": "Javier", "texto": "Hola123", "num_ser": 123456, "activo": 1, "prioridad": "alta"},
-    {"nombre": "Fran", "texto": "Hola123", "num_ser": 123456, "activo": 1, "prioridad": "alta"},
-    {"nombre": "Victor", "texto": "Hola123", "num_ser": 123456, "activo": 1, "prioridad": "alta"},
-    {"nombre": "manuel", "texto": "Hola123", "num_ser": 123456, "activo": 1, "prioridad": "alta"},
-    {"nombre": "vicente", "texto": "Hola123", "num_ser": 123456, "activo": 1, "prioridad": "alta"},
+
 ];
+fetch('ws/getElement.php')
+    .then(res => res.json())
+    .then(data => {
+        //aqui cogemos y recorremos el array de objetos de la base de datos
+        for (let i = 0; i < data.data.length; i++) {
+            elementos.push(data.data[i]);
+        }
+    })
+
+console.log(elementos);
 
 function table_load() {
     let table = document.getElementById("table");
@@ -15,15 +21,24 @@ function table_load() {
 
     cabeceraTabla(table);
     rellenarTabla(table);
-    
+
 }
 
+
+
+
 function rellenarTabla(table) {
-    for(let i = 0; i<elementos.length; i++){
+    for (let i = 0; i < elementos.length; i++) {
         //Creo la fila y sus celdas
-        let elemento = elementos[i];
+        let data = elementos[i];
         let tr = document.createElement('tr');
         tr.id = i;
+        let id = data.id;
+        let nom = data.nombre;
+        let des = data.descripcion;
+        let nser = data.nserie;
+        let est = data.estado;
+        let pri = data.prioridad;
         let tdNombre = document.createElement('td');
         let tdDescripcion = document.createElement('td');
         let tdSerie = document.createElement('td');
@@ -34,30 +49,70 @@ function rellenarTabla(table) {
         let botoned = document.createElement('button');
         let botonguar = document.createElement('button');
         //Le doy un valor a las celdas
-        tdNombre.innerHTML = elemento.nombre;
-        tdDescripcion.innerHTML = elemento.texto;
-        tdSerie.innerHTML = elemento.num_ser;
-        tdActivo.innerHTML = elemento.activo;
-        tdPrioridad.innerHTML = elemento.prioridad;
+        tdNombre.innerHTML = data.nombre;
+        tdDescripcion.innerHTML = data.descripcion;
+        tdSerie.innerHTML = data.nserie;
+        tdActivo.innerHTML = data.estado;
+        tdPrioridad.innerHTML = data.prioridad;
 
         botoned.innerHTML = "Editar";
         botonguar.innerHTML = "Guardar";
-        botonguar.hidden=true;
+        botonguar.hidden = true;
         boton.innerHTML = " <img class='img'  src=\"./img/el.png\"/>";
         boton.onclick = function () {
-            Borrar(i);
+
+            Borrar(i, id, nom, nser, des, est, pri);
         };
         botoned.onclick = function () {
             edita(i);
             //esto es para ocultar el boton editar y mostrar el guardar
-            botoned.hidden=true;
-            botonguar.hidden=false;
+            botoned.hidden = true;
+            botonguar.hidden = false;
         };
         botonguar.onclick = function () {
-            guarda(i);
-            //esto es para ocultar el boton editar y mostrar el guardar
-            botonguar.hidden=true;
-            botoned.hidden=false;
+
+            const swalWithBootstrapButtons = Swal.mixin({
+                customClass: {
+                    confirmButton: 'btn btn-success',
+                    cancelButton: 'btn btn-danger'
+                },
+                buttonsStyling: false
+            })
+
+            swalWithBootstrapButtons.fire({
+                title: 'Seguro que quieres editar?',
+                text: "Si editas modificaras el contenido de este!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Si',
+                cancelButtonText: 'No',
+                reverseButtons: true
+            })
+                .then((result) => {
+                    //guarda(i);
+
+
+                    //esto es para ocultar el boton editar y mostrar el guardar
+                    botonguar.hidden = true;
+                    botoned.hidden = false;
+                    //let res = editarGuar(id,fromDat);
+                    //console.log(res);
+                    if (result.isConfirmed) {
+                        let fromDat = guarda(i);
+                        editarGuar(id, fromDat);
+                    } else if (
+                        /* Read more about handling dismissals below */
+                        result.dismiss === Swal.DismissReason.cancel
+                    ) {
+                        swalWithBootstrapButtons.fire(
+                            'Cancelado',
+                            'No se ha editado el usuario :)',
+                            'error'
+                        )
+                        location.reload();
+                    }
+                })
+
         };
         tdBorrar.appendChild(boton);
         //Añado al tr los td
@@ -95,83 +150,247 @@ function cabeceraTabla(table) {
     tr.appendChild(thActivo);
     tr.appendChild(thPrioridad);
     tr.appendChild(thBorrar);
-    
+
     table.appendChild(tr);
 }
 
-function Borrar(i){
-    let id_lin = document.getElementById(i);
-    id_lin.remove();
+function Borrar(i, id, nom, des, nser, est, pri) {
+    Swal.fire({
+        title: 'Estas seguro de eliminar?',
+        text: "Si lo eliminas ya no aparecera mas",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire(
+                'Eliminado!',
+                'Eliminado correctamente',
+                'success'
+            )
+            id = parseInt(id);
+            let fromDat = new FormData();
+            fromDat.append('nombre', nom);
+            fromDat.append('texto', des);
+            fromDat.append('num_ser', nser);
+            fromDat.append('activo', est);
+            fromDat.append('prioridad', pri);
+
+            fetch(`ws/deleteElement.php?id=${id}`, {
+                method: 'post',
+                body: fromDat,
+            }).then(function (response) {
+                return response.json();
+            }).then(function (data) {
+                console.log(data);
+            });
+            let id_lin = document.getElementById(i);
+            id_lin.remove();
+        }
+    })
 }
 
-    
-function buscar(){ 
+function Crear() {
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+            confirmButton: 'btn btn-success',
+            cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+    })
+
+    swalWithBootstrapButtons.fire({
+        title: 'quieres crear un usuario?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Si, Crear!',
+        cancelButtonText: 'No, cancelar!',
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+
+
+            let nom = document.getElementById("nom").value;
+            let des = document.getElementById("des").value;
+            let nums = document.getElementById("nums").value;
+            let est;
+            let prioridad;
+            if (nom == null) {
+                swalWithBootstrapButtons.fire(
+                    'Creado!',
+                    'El usuario se ha creado',
+                    'success'
+                )
+            }
+            //comprobamos el estado
+            if (document.getElementById("est").checked) {
+                est = "activado";
+            } else {
+                est = "desactvado";
+            }
+
+            //comprobamos la prioridad
+            if (document.getElementById("alta").checked) {
+                prioridad = "alta";
+            } else {
+                if (document.getElementById("media").checked) {
+                    prioridad = "media";
+                } else {
+                    if (document.getElementById("media").checked) {
+                        prioridad = "baja";
+                    }
+                }
+            }
+            let fromDat = new FormData();
+            fromDat.append('nombre', nom);
+            fromDat.append('texto', des);
+            fromDat.append('num_ser', nums);
+            fromDat.append('activo', est);
+            fromDat.append('prioridad', prioridad);
+            fetch("ws/createElement2.php", {
+                method: "POST",
+                body: fromDat
+            })
+                .then((res) => {
+                    if (res.ok) {
+                        console.log("bien");
+                    } else {
+                        console.log("mal");
+                    }
+                    return res;
+                })
+                .then((res) => res.json())
+        } else if (
+            /* Read more about handling dismissals below */
+            result.dismiss === Swal.DismissReason.cancel
+        ) {
+            swalWithBootstrapButtons.fire(
+                'Cancelado',
+                'No se ha creado el usuario',
+                'error'
+            )
+        }
+    })
+
+}
+
+function editarGuar(id, fromDat) {
+    console.log(fromDat);
+    fetch(`ws/modifyElements.php?id=${id}`, {
+        method: 'post',
+        body: fromDat,
+    })
+        .then((response) => response.json())
+        .then((response) => {
+            if (response.success) {
+                Swal.fire(
+                    'Editado!',
+                    'Tu usuario con id ' + response.data.id + ' sido editado',
+                    'success'
+                )
+            } else {
+                Swal.fire(
+                    'No Editado!',
+                    'Tienes que rellenar todos los campos',
+                    'error'
+                )
+            }
+
+        });
+
+}
+
+function buscar() {
     const searchText = document.getElementById('searchTerm').value.toLowerCase().trim();
-    for(let i = 0; i<elementos.length; i++){
+    for (let i = 0; i < elementos.length; i++) {
         let tr = document.getElementById(i);
         if (searchText.length >= 3) {
-            if(elementos[i].nombre.toLocaleLowerCase().includes(searchText) || elementos[i].texto.toLocaleLowerCase().includes(searchText)){
-                tr.style.backgroundColor="#000000";
-            }else{
-                tr.style.backgroundColor="#23232300";
+            if (elementos[i].nombre.toLocaleLowerCase().includes(searchText) || elementos[i].descripcion.toLocaleLowerCase().includes(searchText)) {
+                tr.style.backgroundColor = "#000000";
+            } else {
+                tr.style.backgroundColor = "#23232300";
             }
-        }else{
-            tr.style.backgroundColor="#23232300";
+        } else {
+            tr.style.backgroundColor = "#23232300";
         }
     }
 }
 
 function edita(i) {
     let celda = document.getElementById(i);
-    for(j=0;j<5;j++){
+    for (j = 0; j < 5; j++) {
         let celdaj = celda.getElementsByTagName('td')[j];
         let txt = celdaj.innerHTML;
         celdaj.innerHTML = '';
         let inp = celdaj.appendChild(document.createElement('input'));
-        inp.value=txt;
-    //el intento del 10 no se ha podido
-    /*
-    if(j==3){
-        inpc = celdaj.appendChild(document.createElement('input'));
-        inpc.type="checkbox";
-        selectList.id = "che";
-    }else if(j==4){
-        array = ["Alta","Media","Baja"];
-        selectList = document.createElement("select");
-        selectList.id = "mySelect";
-        myParent.appendChild(selectList);
-        for (i = 0; i < array.length; i++) {
-            opcion = document.createElement("opcion");
-            opcion.value = array[i];
-            opcion.text = array[i];
-            selectList.appendChild(opcion);
-        }
-        
-    }else{*/
+        inp.value = txt;
+        //el intento del 10 no se ha podido
+        /*
+        if(j==3){
+            inpc = celdaj.appendChild(document.createElement('input'));
+            inpc.type="checkbox";
+            selectList.id = "che";
+        }else if(j==4){
+            array = ["Alta","Media","Baja"];
+            selectList = document.createElement("select");
+            selectList.id = "mySelect";
+            myParent.appendChild(selectList);
+            for (i = 0; i < array.length; i++) {
+                opcion = document.createElement("opcion");
+                opcion.value = array[i];
+                opcion.text = array[i];
+                selectList.appendChild(opcion);
+            }
+            
+        }else{*/
     }
 }
+
 function guarda(i) {
     let celda = document.getElementById(i);
-    for(j=0;j<5;j++){
-    let celdaj = celda.getElementsByTagName('td')[j];
-    let inputj = celda.getElementsByTagName('td')[j].firstChild;
-    /*
-   if(j==3){
-        if(document.getElementById('che').checked){
-            inputj.value=1;
-        }else{
-            inputj.value=0;
+    let fromDat = new FormData();
+
+    for (j = 0; j < 5; j++) {
+        let celdaj = celda.getElementsByTagName('td')[j];
+        let inputj = celda.getElementsByTagName('td')[j].firstChild;
+        /*
+        if(j==3){
+            if(document.getElementById('che').checked){
+                inputj.value=1;
+            }else{
+                inputj.value=0;
+            }
+        }*/
+        let txt = inputj.value;
+        switch (j) {
+            case 0:
+                fromDat.append('nombre', txt);
+                break;
+            case 1:
+                fromDat.append('texto', txt);
+                break;
+            case 2:
+                fromDat.append('num_ser', txt);
+                break;
+            case 3:
+                fromDat.append('activo', txt);
+                break;
+            case 4:
+                fromDat.append('prioridad', txt);
+                break;
         }
-    }*/
-    let txt = inputj.value;
-    celdaj.innerHTML = txt;
-    inputj.remove;
+        celdaj.innerHTML = txt;
+        inputj.remove();
     }
     let count = 0;
-    for(let key in elementos[i]) {
-        elementos[i][key] = celda.getElementsByTagName('td')[count].innerHTML; 
-        count ++;
+    for (let key in elementos[i]) {
+        elementos[i][key] = celda.getElementsByTagName('td')[count].innerHTML;
+        count++;
     }
+    return fromDat;
 }
-    
-  
+
+
+
